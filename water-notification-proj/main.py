3c285d35e287e9b013e57f1e2c440febb862930d
Malla Asha <63631167+Asha-Malla@users.noreply.github.com>
2021-07-27 07:55:38 +0530

    Add files via upload

---
+++ water-notification-proj/main.py
@@ -0,0 +1,10 @@
+import time
+from plyer import notification
+if __name__ == '__main__':
+    notification.notify(
+        title="DRINK A GLASS OF WATER",
+        message="Regular drinking of water is good for health and keeps body hydrated.",
+        # downloaded from https://icon-icons.com/
+        app_icon="E:\projects\water-notification-proj\water glass.ico",
+        timeout=5
+    )
