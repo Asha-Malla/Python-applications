926711e1f371d9c927db2c52ea097f23444c41f7
Malla Asha <63631167+Asha-Malla@users.noreply.github.com>
2021-08-03 21:10:25 +0530

    Update main.py

--- water-notification-proj/main.py
+++ water-notification-proj/main.py
@@ -1,10 +1,14 @@
 import time
 from plyer import notification
 if __name__ == '__main__':
-    notification.notify(
-        title="DRINK A GLASS OF WATER",
-        message="Regular drinking of water is good for health and keeps body hydrated.",
-        # downloaded from https://icon-icons.com/
-        app_icon="E:\projects\water-notification-proj\water glass.ico",
-        timeout=5
-    )
+    while True:
+        notification.notify(
+            title="DRINK A GLASS OF WATER",
+            message="Regular drinking of water is good for health and keeps body hydrated.",
+            # downloaded from https://icon-icons.com/
+            app_icon="E:\projects\water-notification-proj\water glass.ico",
+            timeout=5
+        )
+        time.sleep(3600)
+# Run using command - pythonw .\main.py
+# Runs in background
